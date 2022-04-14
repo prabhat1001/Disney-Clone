fbcfea6aae15c6034af9c2003ffb1093b749c24a
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-04-14 21:31:12 +0530

    App.js updates

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header element={<Header />}/>
         <Routes> {/*In react-router-dom v6, "Switch" is replaced by routes "Routes". */}
           <Route exact path="/" element={<Login />}>
           </Route>
