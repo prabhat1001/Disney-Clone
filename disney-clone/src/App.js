292331b3da9f665f241589b14bc51b7b07ab32ec
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-04-14 12:16:00 +0530

    here it is!

---
+++ disney-clone/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./components/Login";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes> {/*In react-router-dom v6, "Switch" is replaced by routes "Routes". */}
+          <Route exact path ='/'>
+            <Login/>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
