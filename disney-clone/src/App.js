f14d0e40f5a3a2841c7f358d6f341b7ab3d3c202
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-05-19 00:51:32 +0530

    Home page Route added

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header"
 import './App.css';
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
       <Router>
         <Header element={<Header />}/>
         <Routes> {/*In react-router-dom v6, "Switch" is replaced by routes "Routes". */}
-          <Route exact path="/" element={<Login />}>
-          </Route>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route path ='/home' element={<Home/>}></Route> 
         </Routes>
       </Router>
     </div>
