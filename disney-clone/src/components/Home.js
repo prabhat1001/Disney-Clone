153d76b2cc65552e0024372652cfcc1f5bb74556
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-05-22 19:25:47 +0530

    home.js updated

--- disney-clone/src/components/Home.js
+++ disney-clone/src/components/Home.js
@@ -1,11 +1,45 @@
 import styled from 'styled-components';
-
+import ImgSlider from './ImgSlider';
+import NewDisney from './NewDisney';
+import Originals from './Originals';
+import Recommends from './Reccomends';
+import Trending from './Trending';
+import Viewers from './Viewers';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName } from '../features/user/userSlice';
 const Home = (props) =>{
     return(
-        <div>
-            Home
-        </div>
+        <Container>
+            <ImgSlider/>
+            <Viewers/>
+            <Recommends/>
+            <NewDisney/>
+            <Originals/>
+            <Trending/>
+        </Container>
     )
 }
+
+const Container = styled.main`
+position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+    &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+
+`
 
 export default Home;
